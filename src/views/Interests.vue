12a6edb163ea7421584e287618ace03802283183
Ghostpupper <9207gusmat@gmail.com>
2021-02-07 12:39:09 +0100

    The tailwind plugin totally screwed things up

---
+++ src/views/Interests.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container>
+    <h1>This is the interests page</h1>
+  </v-container>
+</template> 
---
+++ src/views/Resume.vue
@@ -0,0 +1,6 @@
+
+<template>
+  <v-container>
+    <h1>This is my Resume</h1>
+  </v-container>
+</template> 
