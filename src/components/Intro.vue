ddbbd76a58ea48815dd70fdbbc89ce4565eb74c1
Ghostpupper <62701971+Ghostpupper@users.noreply.github.com>
2021-03-13 23:31:58 +0100

    Edit resume (#14)
    * small touches
    
    * intro to resume got changed back

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -36,7 +36,7 @@
           >
             <base-heading>My Resume!</base-heading>
             <base-text>
-              Im a playful and social for being a software engineer. But I also take great pride in my work and ensure the things I work on has high quality.<br>
+              I thrive to work in a well connected tight team where everyone can bring their inputs to the table. Its great to be in a "T-shaped" role where I can be free to explore a wide range of tasks and technologies.<br>
               Im currently a Software Integration Engineer and a Scrum Master at a large heavy duty Vehicle Company.
             </base-text>
           </v-flex>
@@ -49,4 +49,3 @@
     </v-layout>
   </section>
 </template>
-
